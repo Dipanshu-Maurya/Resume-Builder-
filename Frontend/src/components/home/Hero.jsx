47610fc97bbe51acea64dcb1bb32298d2057f0ff
Dipanshu-Maurya <dipanshumaurya2005@gmail.com>
2026-01-29 11:12:48 +0530

    added login and logout functionality

--- Frontend/src/components/home/Hero.jsx
+++ Frontend/src/components/home/Hero.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link, Links } from "react-router-dom";
 
 const Hero = () => {
+
+  const {user} = useSelector(state => state.auth)
+
   const [menuOpen, setMenuOpen] = React.useState(false);
 
   const companiesLogo = [
@@ -138,15 +142,17 @@ const Hero = () => {
           <div className="flex gap-2">
             <Link
               to='/app?state=register'
-              className="hidden md:block px-6 py-2 bg-green-500 hover:bg-green-700 active:scale-95 transition-all rounded-full text-white"
+              className="hidden md:block px-6 py-2 bg-green-500 hover:bg-green-700 active:scale-95 transition-all rounded-full text-white" hidden={user}
             >
               Get started
             </Link>
-            <Link to='/app.state=login'
-              href=""
-              className="hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900"
+            <Link to='/app?state=login'
+              className="hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900" hidden={user}
             >
               Login
+            </Link>
+            <Link to='/app' className="hidden md:block px-8 py-2 bg-green-500 hover:bg-green-700 active:scale-95 transition-all rounded-full text-white"  hidden={!user}>
+              Dashboard
             </Link>
           </div>
 
